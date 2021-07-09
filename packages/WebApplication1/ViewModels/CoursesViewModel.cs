c421cbef238086d22af0ba213f277c0f06fa64ca
tungpaxcan <nguyennhutung999@gmail.com>
2021-07-09 11:22:26 +0700

    first commit

---
+++ packages/WebApplication1/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
