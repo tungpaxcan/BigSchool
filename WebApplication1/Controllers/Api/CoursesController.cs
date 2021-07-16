2513bd8f979e2f46c61d25222eb62ee1f8e6abf3
tungpaxcan <nguyennhutung999@gmail.com>
2021-07-16 08:19:25 +0700

    first commit

--- WebApplication1/Controllers/Api/CoursesController.cs
+++ WebApplication1/Controllers/Api/CoursesController.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers.Api
 {
-    public class CoursesController : Controller
+    public class CoursesController : ApiController
     {
-        // GET: Courses
-        public ActionResult Index()
+        public ApplicationDbContext _dbContext { get; set; }
+        public CoursesController()
         {
-            return View();
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+            if (course.IsCanceled)
+                return NotFound();
+            course.IsCanceled = true;
+            _dbContext.SaveChanges();
+            return Ok();
         }
     }
 }
