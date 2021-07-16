58f3655cdecab956444dd8dac0c9dbfd6b00eb42
tungpaxcan <nguyennhutung999@gmail.com>
2021-07-16 10:29:02 +0700

    first commit

--- WebApplication1/Controllers/FollowingsController.cs
+++ WebApplication1/Controllers/FollowingsController.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.DTOs;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class FollowingsController : Controller
+    public class FollowingsController : ApiController
     {
-        // GET: Followings
-        public ActionResult Index()
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingsController()
         {
-            return View();
+            _dbContext = new ApplicationDbContext();
         }
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+                return BadRequest("Following already exists!");
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(following);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
     }
 }
